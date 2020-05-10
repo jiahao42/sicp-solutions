97f87d1f63f04dc585e976f669dcb7e728a093ac
Jiahao Cai <jiahao.cai42@gmail.com>
2020-05-10 16:36:49 -0400

    3.21

--- sicp-ch.3.rkt
+++ sicp-ch.3.rkt
@@ -469,3 +469,51 @@ zz
 
 (optimized-circle-detector (make-cycle (mlist 'a 'b 'c)))
 (optimized-circle-detector (mlist 1 2 3))
+
+(say "Exercise 3.21")
+
+(define (front-ptr queue) (mcar queue))
+(define (rear-ptr queue) (mcdr queue))
+(define (set-front-ptr! queue item) (set-car! queue item))
+(define (set-rear-ptr! queue item) (set-cdr! queue item))
+(define (empty-queue? queue) (null? (front-ptr queue)))
+(define (make-queue) (mcons '() '()))
+(define (front-queue queue)
+  (if (empty-queue? queue)
+    (error "FRONT called with an empty queue" queue)
+    (mcar (front-ptr queue))))
+(define (insert-queue! queue item)
+  (let ((new-pair (mcons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+           (else 
+             (set-cdr! (rear-ptr queue) new-pair)
+             (set-rear-ptr! queue new-pair)
+             queue))))
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "DELETE! called with an empty queue" queue))
+        (else (set-front-ptr! queue (mcdr (front-ptr queue)))
+              queue)))
+
+(define (print-queue queue)
+  (let ((front (front-ptr queue)))
+    (define (print-queue-aux ptr)
+      (if (mpair? ptr) 
+           (begin
+             (display (mcar ptr))
+             (display "->")
+             (print-queue-aux (mcdr ptr)))
+        (say "null")))
+    (print-queue-aux front)))
+
+
+(define q1 (make-queue))
+(print-queue (insert-queue! q1 'a))
+(print-queue (insert-queue! q1 'b))
+(print-queue (delete-queue! q1))
+(print-queue (delete-queue! q1))
+
+
