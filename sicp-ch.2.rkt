01422e9fcba32ba98925d55a46916cc9d22ea40f
Jiahao Cai <jiahao.cai42@gmail.com>
2020-05-14 12:44:29 -0400

    2.26

--- sicp-ch.2.rkt
+++ sicp-ch.2.rkt
@@ -368,3 +368,10 @@ zero
 (car (cdr (car (cdr (cdr (list 1 3 (list 5 7) 9))))))
 (car (car (list (list 7))))
 (cadr (cadr (cadr (cadr (cadr (cadr (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7))))))))))))
+
+(say "Exercise 2.26")
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+(append x y)
+(cons x y)
+(list x y)
