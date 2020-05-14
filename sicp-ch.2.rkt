645209a17aab48ff778232147856a7edb172e8f8
Jiahao Cai <jiahao.cai42@gmail.com>
2020-05-14 12:43:19 -0400

    2.25

--- sicp-ch.2.rkt
+++ sicp-ch.2.rkt
@@ -364,4 +364,7 @@ zero
 ;         3   4
 ;
 
-
+(say "Exercise 2.25")
+(car (cdr (car (cdr (cdr (list 1 3 (list 5 7) 9))))))
+(car (car (list (list 7))))
+(cadr (cadr (cadr (cadr (cadr (cadr (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7))))))))))))
