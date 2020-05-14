b51a3e937f58af6bf454a6ec3c036d2ed32aedf8
Jiahao Cai <jiahao.cai42@gmail.com>
2020-05-14 01:57:01 -0400

    2.21

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 This repo. contains my solutions of SICP in Racket.
 
-Note that the solutions of Chapter 1-2 was done in 2018, so they could be very unmatured :)
+Note that the solutions of Chapter 1 and part of Chapter 2 was done in 2018, so they could be very unmatured :)
 
 
--- sicp-ch.2.rkt
+++ sicp-ch.2.rkt
@@ -314,4 +314,19 @@ zero
 
 (same-parity 1 2 3 4 5 6 7 8 9)
       
+(define (say msg) (display msg) (display "\n"))
+
+(say "Exercise 2.21")
+(define (square-list1 items)
+  (if (null? items)
+    '()
+    (cons (* (car items) (car items)) (square-list1 (cdr items)))))
+(square-list1 (list 1 2 3 4))
+
+(define (square-list2 items)
+  (map (λ (x) (* x x)) items))
+(square-list2 (list 1 2 3 4))
+
+
+
 
