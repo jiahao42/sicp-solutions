03494151fc38ae821db13a0a743311e89cad22b8
Jiahao Cai <jiahao.cai42@gmail.com>
2020-05-14 12:51:14 -0400

    2.27

--- sicp-ch.2.rkt
+++ sicp-ch.2.rkt
@@ -372,6 +372,19 @@ zero
 (say "Exercise 2.26")
 (define x (list 1 2 3))
 (define y (list 4 5 6))
-(append x y)
-(cons x y)
-(list x y)
+(append x y) ; '(1 2 3 4 5 6)
+(cons x y) ; '((1 2 3) 4 5 6)
+(list x y) ; '((1 2 3) (4 5 6))
+
+(say "Exercise 2.27")
+
+(define (deep-reverse l)
+ (if (null? l)
+     l
+     (append (deep-reverse 
+               (cdr l)) 
+             (cons (reverse (car l)) '()))))
+(reverse (list (list 1 2) (list 3 4)))
+(deep-reverse (list (list 1 2) (list 3 4)))
+
+
