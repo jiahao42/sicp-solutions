892fd965259f4c700bc2236078b8d2d3d2dea6d7
Jiahao Cai <jiahao.cai42@gmail.com>
2020-05-15 02:30:03 -0400

    2.30

--- sicp-ch.2.rkt
+++ sicp-ch.2.rkt
@@ -516,7 +516,21 @@ tree2
 (balanced-alt? mobile-alt)
 (balanced-alt? (make-mobile-alt sub-mobile-alt sub-mobile-alt))
 
-
+(say "Exercise 2.30")
+(define (square-tree tree)
+  (cond ((null? tree) '())
+        ((not (pair? tree)) (square tree))
+        (else
+          (cons (square-tree (car tree))
+                (square-tree (cdr tree))))))
+(square-tree (list 1 (list 2 (list 3 4) 5) (list 6 7)))
+(define (square-tree-map tree)
+  (map (λ (sub-tree)
+       (if (pair? sub-tree)
+         (square-tree-map sub-tree)
+         (square sub-tree)))
+  tree))
+(square-tree-map (list 1 (list 2 (list 3 4) 5) (list 6 7)))
 
 
 
