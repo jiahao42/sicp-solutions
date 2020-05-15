fd357b64cc6dabe6ca777a6560dc70010b033380
Jiahao Cai <jiahao.cai42@gmail.com>
2020-05-15 13:42:48 -0400

    2.33

--- sicp-ch.2.rkt
+++ sicp-ch.2.rkt
@@ -557,6 +557,21 @@ tree2
 ; when iterating at 2: ('() (3) (2) (2 3))
 ; when iterating at 1: ('() (3) (2) (2 3) (1) (1 3) (1 2) (1 2 3))
 
+(say "Exercise 2.33")
+(define (my-accumulate op initial sequence)
+  (if (null? sequence)
+    initial
+    (op (car sequence)
+        (my-accumulate op initial (cdr sequence)))))
+(define (my-map p sequence)
+  (my-accumulate (λ (x y) (cons (p x) y)) '() sequence))
+(my-map square (list 1 2 3))
+(define (my-append seq1 seq2)
+  (my-accumulate cons seq2 seq1))
+(my-append (list 1 2 3) (list 4 5 6))
+(define (my-length sequence)
+  (my-accumulate (λ (x y) (+ y 1)) 0 sequence))
+(my-length (list 1 2 3))
 
 
 
