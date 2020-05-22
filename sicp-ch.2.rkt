636bc13ce5d46c88490071a76871dd34b2bafe15
Jiahao Cai <jiahao.cai42@gmail.com>
2020-05-22 01:30:27 -0400

    2.52

--- sicp-ch.2.rkt
+++ sicp-ch.2.rkt
@@ -825,15 +825,6 @@ tree2
            (bottom (beside (bl painter) (br painter))))
        (below bottom top))))
 
-;(define (flipped-pairs painter)
-  ;(let ((combine4 (square-of-four identity flip-vert
-                                  ;identity flip-vert)))
-    ;(combine4 painter)))
-
-;(define (square-limit painter n)
-  ;(let ((combine4 (square-of-four flip-horiz identity
-                                  ;rotate180 flip-vert)))
-    ;(combine4 (corner-split painter n))))
 (define (split dir1 dir2)
   (define (split-internal painter n)
     (if (= n 0)
@@ -1090,6 +1081,37 @@ segment
 ;(paint (my-below-alt T T))
 
 
+(say "Exercise 2.52")
+; 2.52.a 
+; Too lazy to draw
 
+; 2.52.b
+(define (right-split-alt painter n)
+  (if (= n 0)
+    painter
+    (let ((smaller (right-split-alt painter (- n 1))))
+      (beside painter smaller))))
+(define (up-split-alt painter n)
+  (if (= n 0)
+    painter
+    (let ((smaller (up-split-alt painter (- n 1))))
+      (below painter smaller))))
+(define (corner-split-alt painter n)
+  (if (= n 0)
+    painter
+    (let ((up (up-split-alt painter (- n 1)))
+          (right (right-split-alt painter (- n 1))))
+      (let ((top-left (beside up up))
+            (bottom-right (below right right))
+            (corner (corner-split-alt painter (- n 1))))
+        (beside (below painter top-left)
+                (below bottom-right corner))))))
+;(paint (corner-split-alt wave 4))
 
+; 2.52.c
+(define (square-limit-alt painter n)
+  (let ((quarter (rotate180 (corner-split painter n))))
+    (let ((half (beside (flip-horiz quarter) quarter)))
+      (below (flip-vert half) half))))
+(paint (square-limit-alt wave 4))
 
