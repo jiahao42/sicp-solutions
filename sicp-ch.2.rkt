9e3e28b85187cca3fb58fc45b455a1ed464efbb8
Jiahao Cai <jiahao.cai42@gmail.com>
2020-05-23 19:36:21 -0400

    2.60

--- sicp-ch.2.rkt
+++ sicp-ch.2.rkt
@@ -1393,5 +1393,22 @@ set2
 (intersection-set set1 set2)
 (union-set set1 set2)
 
-
-
+(say "Exercise 2.60")
+(define (Exercise-2-60)
+  (define (element-of-set? x set)
+    (cond ((null? set) #f)
+          ((equal? x (car set)) #t)
+          (else (element-of-set? x (cdr set)))))
+  (define (adjoin-set x set)
+      (cons x set))
+  (define (intersection-set set1 set2)
+    (cond ((or (null? set1) (null? set2)) '())
+          ((element-of-set? (car set1) set2)
+           (cons (car set1) (intersection-set (cdr set1) set2)))
+          (else (intersection-set (cdr set1) set2))))
+  (define set1 (adjoin-set 1 (adjoin-set 2 (adjoin-set 3 '()))))
+  (define set2 (adjoin-set 3 (adjoin-set 4 (adjoin-set 5 '()))))
+  set1 
+  set2
+  (intersection-set set1 set2))
+(Exercise-2-60)
